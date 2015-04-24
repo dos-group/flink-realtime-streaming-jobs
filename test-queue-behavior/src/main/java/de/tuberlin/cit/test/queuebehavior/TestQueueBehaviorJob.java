9164e1bc855edd2f8b5d9316009e59c876ed56a3
Sascha Wolke <dersascha@users.noreply.github.com>
2015-04-24 22:12:05 +0200

    code reformat

--- test-queue-behavior/src/main/java/de/tuberlin/cit/test/queuebehavior/TestQueueBehaviorJob.java
+++ test-queue-behavior/src/main/java/de/tuberlin/cit/test/queuebehavior/TestQueueBehaviorJob.java
@@ -16,16 +16,16 @@ import java.util.Arrays;
 /**
  * This Flink job is intended for non-interactive cluster experiments with
  * Flink and designed to observe the effects of queueing.
- * 
+ *
  * This Flink job generates large numbers (which is a fairly cheap operation)
  * at an increasing rate and tests them for primeness (which is compute
  * intensive).
- * 
+ *
  * @author Bjoern Lohrmann
  * @author Sascha Wolke
  */
 public class TestQueueBehaviorJob {
-    private final static Logger log = LoggerFactory.getLogger(TestQueueBehaviorJob.class);
+	private final static Logger log = LoggerFactory.getLogger(TestQueueBehaviorJob.class);
 
 	public static void main(final String[] args) throws Exception {
 
@@ -50,25 +50,25 @@ public class TestQueueBehaviorJob {
 
 		String latencyLogfile = args[2];
 
-        final StreamExecutionEnvironment env;
+		final StreamExecutionEnvironment env;
 
-        if (jmHost.equalsIgnoreCase("local")) {
-            log.info("Running in local mode.");
-            env = StreamExecutionEnvironment.getExecutionEnvironment();
+		if (jmHost.equalsIgnoreCase("local")) {
+			log.info("Running in local mode.");
+			env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        } else {
-            log.info("Running in remote mode. Creating jar...");
+		} else {
+			log.info("Running in remote mode. Creating jar...");
 
-            // Create jar file for job deployment
-            Process p = Runtime.getRuntime().exec("mvn clean package");
-            if (p.waitFor() != 0) {
-                System.out.println("Failed to build test-queue-behavior.jar");
-                System.exit(1);
-            }
+			// Create jar file for job deployment
+			Process p = Runtime.getRuntime().exec("mvn clean package");
+			if (p.waitFor() != 0) {
+				System.out.println("Failed to build test-queue-behavior.jar");
+				System.exit(1);
+			}
 
-            env = StreamExecutionEnvironment.createRemoteEnvironment(
-                    jmHost, jmPort, "target/test-queue-behavior-git.jar");
-        }
+			env = StreamExecutionEnvironment.createRemoteEnvironment(
+					jmHost, jmPort, "target/test-queue-behavior-git.jar");
+		}
 
 
 		// optional: env.setQosStatisticsReportInterval(12345);
@@ -83,12 +83,12 @@ public class TestQueueBehaviorJob {
 				.finishLatencyConstraint()
 				.addSink(new LatencyLoggerSink(latencyLogfile)).setParallelism(profile.paraProfile.outerTaskDop);
 
-        env.execute("Test Queue Behavior job");
-    }
+		env.execute("Test Queue Behavior job");
+	}
 
-    private static void printUsage() {
+	private static void printUsage() {
 		System.err.println("Parameters: <jobmanager-host>:<port> <profile-name> <latency-logfile>");
-        System.err.println("Run local test cluster with ,,local'' as jobmanager host and empty port.");
+		System.err.println("Run local test cluster with ,,local'' as jobmanager host and empty port.");
 		System.err.printf("Available profiles: %s\n",
 				Arrays.toString(TestQueueBehaviorJobProfile.PROFILES.keySet().toArray()));
 	}
